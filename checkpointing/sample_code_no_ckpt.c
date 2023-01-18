fec0a41a1acbaf23a7c2b1b059e395cffb04fe66
Ritu Arora <ritua2@yahoo.com>
2023-01-18 04:35:21 -0600

    Create sample_code_no_ckpt.c
    This code was developed with Trung Nguyen Ba for the ITALC project. It can be compiled and run as follows:
    
    gcc -o sample_code_no_ckpt sample_code_no_ckpt.c 
    ./sample_code_no_ckpt

---
+++ checkpointing/sample_code_no_ckpt.c
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <unistd.h>
+
+ void b (int &);
+ void d (int &);
+int main() {
+   int i1 = 10;
+   int i2 = 20;
+   d(i1);
+   b(i2);
+   b(i1);
+   printf("Final i1 = %d\n", i1);
+   printf("Final i2 = %d\n", i2);
+   return 1;
+}
+void b (int & v) {
+   for(int i =0 ; i < 10; i++) {
+      v+=i;
+      printf("Value changed to %d\n", v );
+      sleep(1);
+   }
+}
+void d (int & v) {
+   for (int i =0; i < 10; i++) {
+      b(v);
+   }
+}
